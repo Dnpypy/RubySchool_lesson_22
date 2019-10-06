18d08c927e86b80a3884ee65a124581ae173aabd
Dnpypy <denlatypoff@yandex.ru>
2019-10-07 02:33:36 +0300

    Add phone, datetime in visit.erb form

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ require 'sinatra/reloader'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!!"			
-	
 end
 
 # обработчик запроса get
@@ -15,6 +14,13 @@ end
 
 get "/visit" do
 	erb :visit
+end
+
+post "/visit" do
+	@login = params[:login]
+	@pass = params[:pass]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
 end
 
 get "/contacts" do
