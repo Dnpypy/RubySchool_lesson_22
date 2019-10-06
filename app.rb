db50f2ad2a85bac5018f5d5f9db0dfdd6148e10c
Dnpypy <denlatypoff@yandex.ru>
2019-10-06 23:27:19 +0300

    Add about pages about and visit

--- app.rb
+++ app.rb
@@ -5,4 +5,21 @@ require 'sinatra/reloader'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!!"			
+	
 end
+
+get "/about" do
+	erb :about
+end
+
+# post "/about" do
+# 	erb :about
+# end
+
+get "/visit" do
+	erb :visit
+end
+
+# post "/visit" do
+# 	erb :visit
+# end
